f26f50393424f2ef5ed5e3fa35fb5da8e8aad8d9
lucianocortese <lucianocortese444@gmail.com>
2019-02-14 12:18:08 -0300

    14/02 12:18

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,16 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 
-var edad;
-edad=document.getElementById("edad").value;
-var estadoCivil;
-estadoCivil=document.getElementById("estadoCivil").value;
+      var edad;
+      edad=document.getElementById("edad").value;
+      var estadoCivil;
+      estadoCivil=document.getElementById("estadoCivil").value;
 
-if(edad<18 && estadoCivil!="Soltero")
-{
+      if(edad<18 && estadoCivil!="Soltero")
+      {
     
-      alert("es muy pequeño para no ser soltero");
+            alert("es muy pequeño para no ser soltero");
 
-}
+      }
+      else
+      {
+            
+      }
 //FIN DE LA FUNCION
 }
